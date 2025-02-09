0eb50adacdff7ea434c854434c1b5000faf16980
bracha-shreiber <b0583270458@gmail.com>
2025-02-09 23:36:43 +0200

    again again

--- src/service.js
+++ src/service.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
-const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, 
-});
+axios.baseURL(process.env.REACT_APP_API_URL)
 axiosInstance.interceptors.response.use(
   response => {
     return response.data;
