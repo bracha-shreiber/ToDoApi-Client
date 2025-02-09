c868d14d553f8a958ed1b774153e9864cdabb39b
bracha-shreiber <b0583270458@gmail.com>
2025-02-09 22:25:51 +0200

    with envirnment

--- src/service.js
+++ src/service.js
@@ -1,17 +1,26 @@
 import axios from 'axios';
-
-const apiUrl = "https://localhost:7271"
-
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL, 
+});
+axiosInstance.interceptors.response.use(
+  response => {
+    return response.data;
+  },
+  error => {
+    console.error('Error occurred:', error.response ? error.response.data : error.message);
+    return Promise.reject(error);
+  }
+);
 export default {
   getTasks: async () => {
-    const result = await axios.get(`${apiUrl}/items`);    
+    const result = await axios.get(`${baseURL}/items`);    
     return result.data;
   },
 
   postTask: async (name) => {
     console.log('addTask', name);
     try {
-      const result = await axios.post(`${apiUrl}/items`, { name });
+      const result = await axios.post(`${baseURL}/items`, { name });
       return result.data; // מחזיר את הנתונים שהתקבלו מהשרת
     } catch (error) {
       console.error('Error adding task:', error);
@@ -22,7 +31,7 @@ export default {
   putCompleted: async (id, isComplete) => {
     console.log('setCompleted', { id, isComplete });
     try {
-      const result = await axios.put(`${apiUrl}/items/${id}`, { isComplete });
+      const result = await axios.put(`${baseURL}/items/${id}`, { isComplete });
       return result.data; // מחזיר את הנתונים שהתקבלו מהשרת
     } catch (error) {
       console.error('Error updating task completion:', error);
@@ -33,7 +42,7 @@ export default {
   deleteTask: async (id) => {
     console.log('deleteTask', id);
     try {
-      await axios.delete(`${apiUrl}/items/${id}`);
+      await axios.delete(`${baseURL}/items/${id}`);
       return { success: true }; // מחזיר אובייקט שמצביע על הצלחה
     } catch (error) {
       console.error('Error deleting task:', error);
